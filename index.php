c85c08869c5947d0f9ed392334f8d4c57e580541
Jan-elle-Chan <j_michelene@yahoo.com>
2023-10-17 12:36:58 -0700

    Add javascript for dropdown

--- index.php
+++ index.php
@@ -4,7 +4,7 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Find a Tutor</title>
-    <link rel="stylesheet" href="css/style.css">
+    <link rel="stylesheet" href="style.css">
   </head>
   <body>
 <header>
@@ -14,8 +14,7 @@
 <nav class="topnav" id="myTopnav">
   <a class="active" href="index.php">Home</a>
   <div class="dropdown">
-    <div class="dropbtn">My Profile 
-  </div>
+    <div class="dropbtn">My Profile</div>
   <div class="dropdown-container">
     <a href="student.php">Students</a>
     <a href="history.php">History</a>
@@ -25,3 +24,37 @@
   <a href="login.php">Login</a>
   <a href="javascript:void(0);" style="font-size:15px;" class="icon" onclick="myFunction()">&#9776;</a>
 </nav>
+
+<main>
+</main>
+
+
+<script>
+function myFunction() {
+  var x = document.getElementById("myTopnav");
+  if (x.className === "topnav") {
+    x.className += " responsive";
+  } else {
+    x.className = "topnav";
+  }
+}
+
+var dropdown = document.getElementsByClassName("dropbtn");
+  var i;
+  
+  for (i = 0; i <script dropdown.length; i++) {
+    dropdown[i].addEventListener("click", function() {
+      this.classList.toggle("active");
+      var dropdownContent = this.nextElementSibling;
+      if (dropdownContent.style.display === "block") {
+        dropdownContent.style.display = "none";
+      } else {
+        dropdownContent.style.display = "block";
+      }
+    });
+  }
+
+</script>
+
+</body>
+</html>
