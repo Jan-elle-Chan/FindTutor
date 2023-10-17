765b74f6bc7ed06b819d823b4f80118300f17d87
Jan-elle-Chan <j_michelene@yahoo.com>
2023-10-17 11:35:37 -0700

    Add navigation items

--- index.php
+++ index.php
@@ -6,3 +6,22 @@
     <title>Find a Tutor</title>
     <link rel="stylesheet" href="css/style.css">
   </head>
+  <body>
+<header>
+    <h1>Find a Tutor</h1>
+</header>
+
+<nav class="topnav" id="myTopnav">
+  <a class="active" href="index.php">Home</a>
+  <div class="dropdown">
+    <div class="dropbtn">My Profile 
+  </div>
+  <div class="dropdown-container">
+    <a href="student.php">Students</a>
+    <a href="history.php">History</a>
+    <a href="report.php">Reports</a>
+    </div>
+  </div> 
+  <a href="login.php">Login</a>
+  <a href="javascript:void(0);" style="font-size:15px;" class="icon" onclick="myFunction()">&#9776;</a>
+</nav>
