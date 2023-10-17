cc6651549004f06b43ab6a73ebf91cf5801202fe
Jan-elle-Chan <j_michelene@yahoo.com>
2023-10-17 14:38:57 -0700

    Modify 4 form fields and style

--- index.php
+++ index.php
@@ -42,23 +42,17 @@
            
 
             <form action="collecting-form-data.php" method="POST">
-            <label>Name:   </label>  <input type="text" name="name">
-            <label>Age:      <input type="text" name="age"></label>
-            <label>Email:    <input type="text" name="email"></label>
-            <label>Password: <input type="password" name="pwd"></label>
-            <label>Bio:      <textarea name="bio"></textarea></label>
-            <label>Contact preference:
-                <select name="preferences">
-                <option value="email">Email</option>
-                <option value="phone">Phone</option>
-                </select></label>
-            <p>Rating: 
-                1 <input type="radio" name="rating" value="1">
-                2 <input type="radio" name="rating" value="2">
-                3 <input type="radio" name="rating" value="3"></p>
-            <p><input type="checkbox" name="terms" value="true"> 
-            I agree to the terms and conditions.</p>
-            <p><input type="submit" value="Save"></p>
+            <label>Subject</label>  
+            <input type="text" name="subject">
+            <label>Level</label>      
+            <input type="text" name="level"></label>
+            <label>Time</label>   
+            <input type="text" name="time"></label>
+                      
+            <label>Session</label>
+            <input type="text" name="sessiontype"></label>
+            
+                <button>Submit</button>
             </form>
 
 
