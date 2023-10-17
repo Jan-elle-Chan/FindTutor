8a2c5153023a55bc27d70eff7d8175a8e99e71b2
Jan-elle-Chan <j_michelene@yahoo.com>
2023-10-17 16:36:27 -0700

    Add image to login page

---
+++ login.php
@@ -0,0 +1,92 @@
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Find a Tutor</title>
+    <link rel="stylesheet" href="style.css">
+  </head>
+  <body>
+<header>
+    <h1>Find a Tutor</h1>
+</header>
+
+<nav class="topnav" id="myTopnav">
+  <a class="active" href="index.php">Home</a>
+  <div class="dropdown">
+    <div class="dropbtn">My Profile</div>
+  <div class="dropdown-container">
+    <a href="student.php">Students</a>
+    <a href="history.php">History</a>
+    <a href="report.php">Reports</a>
+    </div>
+  </div> 
+  <a href="login.php">Login</a>
+  <a href="javascript:void(0);" style="font-size:15px;" class="icon" onclick="myFunction()">&#9776;</a>
+</nav>
+
+<main>
+<section id="animations" class = "animations">
+    <div class = "content page-grid">
+        
+          <!-- Card 1-->
+          <section class = "card">
+            <h3>About Us</h3>
+            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam.</p>
+            <img src="images/pexels-erik-mclean-9258431.jpg" alt="Image of books">
+          </section>
+
+          <!-- Card 2-->
+          <section class = "card">
+            <h3>Form</h3>
+           
+
+            <form action="collecting-form-data.php" method="POST">
+            <label>Subject</label>  
+            <input type="text" name="subject">
+            <label>Level</label>      
+            <input type="text" name="level"></label>
+            <label>Time</label>   
+            <input type="text" name="time"></label>
+                      
+            <label>Session</label>
+            <input type="text" name="sessiontype"></label>
+            
+                <button>Submit</button>
+            </form>
+
+
+          </section>
+</div>
+</main>
+
+
+<script>
+function myFunction() {
+  var x = document.getElementById("myTopnav");
+  if (x.className === "topnav") {
+    x.className += " responsive";
+  } else {
+    x.className = "topnav";
+  }
+}
+
+var dropdown = document.getElementsByClassName("dropbtn");
+  var i;
+  
+  for (i = 0; i <script dropdown.length; i++) {
+    dropdown[i].addEventListener("click", function() {
+      this.classList.toggle("active");
+      var dropdownContent = this.nextElementSibling;
+      if (dropdownContent.style.display === "block") {
+        dropdownContent.style.display = "none";
+      } else {
+        dropdownContent.style.display = "block";
+      }
+    });
+  }
+
+</script>
+
+</body>
+</html>
