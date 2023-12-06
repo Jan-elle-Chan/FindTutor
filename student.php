8b1c1c9bc5a5ff4f139d23b408acb083d77e01e8
Jan-elle-Chan <j_michelene@yahoo.com>
2023-12-05 20:22:32 -0800

    Format student.php

--- student.php
+++ student.php
@@ -1,8 +1,6 @@
 <?php
 require 'includes/database-connection.php';             // Create PDO object
 require 'includes/functions.php';                       // Functions 
-
-
 $sql       = "SELECT first, last, currentLevel FROM student
               WHERE userId ='502';"; // SQL
 $statement = $pdo->query($sql);                                // Run query
@@ -19,45 +17,45 @@ $students   = $statement->fetchAll();                           // Get data
     <link rel="stylesheet" href="style.css">
   </head>
   <body>
-<header>
-    <h1>Find a Tutor</h1>
-</header>
+    <header>
+        <h1>Find a Tutor</h1>
+    </header>
+  <!-- Navigation-->
+  <nav class="topnav" id="myTopnav">
+    <a class="active" href="index.php">Home</a>
+    <div class="dropdown">
+      <div class="dropbtn">My Profile</div>
+    <div class="dropdown-container">
+      <a href="student.php">Students</a>
+      <a href="history.php">History</a>
+      <a href="report.php">Reports</a>
+      </div>
+    </div> 
+    <a href="login.php">Login</a>
+    <a href="javascript:void(0);" style="font-size:15px;" class="icon" onclick="myFunction()">&#9776;</a>
+  </nav>
 
-<nav class="topnav" id="myTopnav">
-  <a class="active" href="index.php">Home</a>
-  <div class="dropdown">
-    <div class="dropbtn">My Profile</div>
-  <div class="dropdown-container">
-    <a href="student.php">Students</a>
-    <a href="history.php">History</a>
-    <a href="report.php">Reports</a>
-    </div>
-  </div> 
-  <a href="login.php">Login</a>
-  <a href="javascript:void(0);" style="font-size:15px;" class="icon" onclick="myFunction()">&#9776;</a>
-</nav>
-
-<main>
-<section>
-    <div class = "content">
-        
-
-          <!-- Card-->
-          <section class = "card">
-          <h3>Your students</h3>
+  <main>
+  <section>
+      <div class = "content">
           
-            <div class = "bio-grid">
-            <?php foreach ($students as $student) { ?>
-              <h6><?= html_escape($student['first'])?>&nbsp;<?= html_escape($student['last']) ?></h6>
-              <p>Current Level: <?= html_escape($student['currentLevel']) ?></p>
-              <button>Delete</button>
-            <?php } ?>
+
+            <!-- Card-->
+            <section class = "card">
+            <h3>Your students</h3>
             
-            </div>
-          
-          </section>
-    </div>
-</main>
+              <div class = "bio-grid">
+              <?php foreach ($students as $student) { ?>
+                <h6><?= html_escape($student['first'])?>&nbsp;<?= html_escape($student['last']) ?></h6>
+                <p>Current Level: <?= html_escape($student['currentLevel']) ?></p>
+                <button>Delete</button>
+              <?php } ?>
+              
+              </div>
+            
+            </section>
+      </div>
+  </main>
 
 
 
