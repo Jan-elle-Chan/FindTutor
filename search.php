3e4f1a34324e38bd4457f341d907e40ac1124de7
Jan-elle-Chan <j_michelene@yahoo.com>
2023-12-05 19:56:53 -0800

    Remove js from all pages

--- search.php
+++ search.php
@@ -75,32 +75,7 @@ $tutors   = $statement->fetchAll();                           // Get data
 </main>
 
 
-<script>
-function myFunction() {
-  var x = document.getElementById("myTopnav");
-  if (x.className === "topnav") {
-    x.className += " responsive";
-  } else {
-    x.className = "topnav";
-  }
-}
 
-var dropdown = document.getElementsByClassName("dropbtn");
-  var i;
-  
-  for (i = 0; i <script dropdown.length; i++) {
-    dropdown[i].addEventListener("click", function() {
-      this.classList.toggle("active");
-      var dropdownContent = this.nextElementSibling;
-      if (dropdownContent.style.display === "block") {
-        dropdownContent.style.display = "none";
-      } else {
-        dropdownContent.style.display = "block";
-      }
-    });
-  }
-
-</script>
 
 </body>
 </html>
