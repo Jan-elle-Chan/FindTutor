f13d40ea3d5c1c56f89325512ed73fb2df176270
Jan-elle-Chan <j_michelene@yahoo.com>
2023-12-05 20:17:42 -0800

    Format login page

--- login.php
+++ login.php
@@ -10,53 +10,43 @@
 <header>
     <h1>Find a Tutor</h1>
 </header>
-
-<nav class="topnav" id="myTopnav">
-  <a class="active" href="index.php">Home</a>
-  <div class="dropdown">
-    <div class="dropbtn">My Profile</div>
-  <div class="dropdown-container">
-    <a href="student.php">Students</a>
-    <a href="history.php">History</a>
-    <a href="report.php">Reports</a>
+  <!-- Navigation-->
+  <nav class="topnav" id="myTopnav">
+    <a class="active" href="index.php">Home</a>
+    <div class="dropdown">
+      <div class="dropbtn">My Profile</div>
+    <div class="dropdown-container">
+      <a href="student.php">Students</a>
+      <a href="history.php">History</a>
+      <a href="report.php">Reports</a>
+      </div>
     </div>
-  </div> 
-  <a href="login.php">Login</a>
-  <a href="javascript:void(0);" style="font-size:15px;" class="icon" onclick="myFunction()">&#9776;</a>
-</nav>
+    <a href="login.php">Login</a>
+    <a href="javascript:void(0);" style="font-size:15px;" class="icon" onclick="myFunction()">&#9776;</a>
+  </nav>
 
-<main>
-<section id="animations" class = "animations">
+  <main>
     <div class = "content page-grid">
-        
-          <!-- Card 1-->
-          <section class = "card">
+        <!-- Card 1-->
+        <section class = "card">
             <h3>Welcome Back</h3>
             <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam.</p>
             <img src="images/pexels-erik-mclean-9258431.jpg" alt="Image of books">
-          </section>
+        </section>
 
-          <!-- Card 2-->
-          <section class = "card">
-            <h3>Log In</h3>
-           
-
+        <!-- Card 2-->
+        <section class = "card">
+          <h3>Log In</h3>
             <form action="login.php" method="POST">
-            <label>Email</label>  
-            <input type="text" name="email"></label>
-            <label>Password</label>  
-            <input type="text" name="password"></label>
-                <button>Log In</button>
-                <a href="signup.php">Not registered, sign up</a>
+              <label>Email</label>
+              <input type="text" name="email"></label>
+              <label>Password</label>
+              <input type="text" name="password"></label>
+                  <button>Log In</button>
+                  <a href="signup.php">Not registered, sign up</a>
             </form>
-
-
-          </section>
-</div>
-</main>
-
-
-
-
+        </section>
+    </div>
+  </main>
 </body>
 </html>
