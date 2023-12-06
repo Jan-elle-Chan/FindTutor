1140a32bf6fcb5be85a4585a0e738fd9d3824161
Jan-elle-Chan <j_michelene@yahoo.com>
2023-12-05 20:30:57 -0800

    Format page-not-found.php

--- page-not-found.php
+++ page-not-found.php
@@ -7,35 +7,30 @@
     <link rel="stylesheet" href="style.css">
   </head>
   <body>
-<header>
-    <h1>Find a Tutor</h1>
-</header>
+    <header>
+        <h1>Find a Tutor</h1>
+    </header>
 
-<nav class="topnav" id="myTopnav">
-  <a class="active" href="index.php">Home</a>
-  <div class="dropdown">
-    <div class="dropbtn">My Profile</div>
-  <div class="dropdown-container">
-    <a href="student.php">Students</a>
-    <a href="history.php">History</a>
-    <a href="report.php">Reports</a>
-    </div>
-  </div> 
-  <a href="login.php">Login</a>
-  <a href="javascript:void(0);" style="font-size:15px;" class="icon" onclick="myFunction()">&#9776;</a>
-</nav>
+    <nav class="topnav" id="myTopnav">
+      <a class="active" href="index.php">Home</a>
+      <div class="dropdown">
+        <div class="dropbtn">My Profile</div>
+      <div class="dropdown-container">
+        <a href="student.php">Students</a>
+        <a href="history.php">History</a>
+        <a href="report.php">Reports</a>
+        </div>
+      </div>
+      <a href="login.php">Login</a>
+      <a href="javascript:void(0);" style="font-size:15px;" class="icon" onclick="myFunction()">&#9776;</a>
+    </nav>
 
-<main>
-<section>
-    <div class = "content page-grid">
-    <h2>Page not found</h2>
-        
-          
-</div>
-</main>
-
-
-
-
+    <main>
+      <section>
+        <div class = "content page-grid">
+            <h2>Page not found</h2>
+        </div>
+      </section>
+    </main>
 </body>
 </html>
