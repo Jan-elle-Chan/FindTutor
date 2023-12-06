7347b8ba392ce276d1222667acbc0c9c4d6e97e4
Jan-elle-Chan <j_michelene@yahoo.com>
2023-12-05 20:41:53 -0800

    Add link to app.js on all pages

--- student.php
+++ student.php
@@ -15,6 +15,7 @@ $students   = $statement->fetchAll();                           // Get data
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Find a Tutor</title>
     <link rel="stylesheet" href="style.css">
+    <script src="app.js"></script>
   </head>
   <body>
     <header>
